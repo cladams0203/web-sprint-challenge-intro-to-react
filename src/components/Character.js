abc0e2c182e93e86d6b73e5c682d3f409a18b478
cladams0203 <cladams0203@gmail.com>
2022-07-11 19:41:41 -0400

    finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+
+import React from "react";
+
+export default function Character(props) {
+  const { name } = props.character;
+  console.log(props);
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Car Data</p>
+    </div>
+  );
+}
